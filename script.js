712eb4ac46f901586df3f91ada8abc99ffe542da
Szilagyimarton <szilagyimarton92@gmail.com>
2023-09-26 13:03:25 +0200

    made logger and another function

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let variable = "kis";
-let variable2 = "macska";
+ function logger (param) {
+  console.log(param)
+}
 
-let number = 5
-let number2 = 10
+logger("kismacska")
 
-//console.log("100" + "2")
-//console.log(100 +2)
-console.log("variable")
-console.log(variable + variable2)
-console.log(number+number2)
+
+
+function addToNumbers(number1,number2){
+    let sum = number1 + number2
+    return sum
+}
+/* let result = addToNumbers(2,4)
+logger(result) */
+
+logger(addToNumbers(5,10))
