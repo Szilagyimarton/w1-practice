7e7e06834df3ff78c98214c3691b6c2f8161250d
Szilagyimarton <szilagyimarton92@gmail.com>
2023-09-29 11:41:28 +0200

    for loop - operators

--- script.js
+++ script.js
@@ -23,25 +23,51 @@ let obj = {                          //Object
   }
 }     
 
-console.log(obj["key"])
+/* console.log(obj["key"])
 console.log(obj.key)
-console.log(obj.key4[1])
+console.log(obj.key4[1]) */
 
 let jsonExample = [
   {
     name: "Gipsz Jakab",
-    age: 40
+    age: 40,
+    "hair-count": 10,
+    "zokni-count": 20
   },
   {
     name : "Példa Péter",
-    age: 28
+    age: 28,
+    "hair-count": 100000,
+    "zokni-count": 12
   },
   {
     name: "John Doe",
-    age: 6
+    age: 6,
+    "hair-count": 0,
+    "zokni-count": 37
+
   }
 
 
 
 ]
-console.log(jsonExample[0].name)
+//console.log(jsonExample[0].name)
+
+/* for(let i = 0; i < jsonExample.length; i++){
+  let user = jsonExample[i]
+  if ( user.age > 18  && user["hair-count"] > 11){
+    console.log(user.name)
+  } else {
+    console.log(`${user.name} nem használhatja a sampont`)
+  }
+
+} */
+for(let i = 0; i < jsonExample.length; i++){
+let user = jsonExample[i]
+
+if(user["zokni-count"] % 2 === 1){
+  console.log(`Sell ${user.name} a single sock`)
+} else {
+  console.log( `${user.name} sadly has exact pair of socks`)
+}
+}
